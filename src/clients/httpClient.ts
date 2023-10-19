a4d9fb7f780cb7ae9fccf5503d06f6bb4d6250c6
Francisco <jscriptcoder@gmail.com>
2023-10-20 01:12:53 +0200

    Fix env var

--- src/clients/httpClient.ts
+++ src/clients/httpClient.ts
@@ -2,7 +2,7 @@ import { VALIDATION_CLOUD_DOMAIN } from '@/constants'
 import { Chain, createPublicClient, http } from 'viem'
 
 const transport = http(
-  `https://${VALIDATION_CLOUD_DOMAIN}/v1/${process.env.VIEM_API_KEY}`
+  `https://${VALIDATION_CLOUD_DOMAIN}/v1/${process.env.NEXT_PUBLIC_VALIDATION_CLOUD_KEY}`
 )
 
 export function getHttpClient(chain: Chain) {
