676f3c5b96c49126e4a433263b763b7bc60f4c53
Francisco <jscriptcoder@gmail.com>
2023-10-19 10:05:55 +0200

    Add DaisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import daisyUIPlugin from 'daisyui'
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyUIPlugin],
 }
 export default config
